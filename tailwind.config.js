3ebbee5983aca39fc0f4084db2b3effe06646ebc
Zainulobiddin <zainiddin893@gmail.com>
2025-01-30 12:17:19 +0500

    finish nav

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: ["./*.html"],  // Adjust based on your project structure
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  };
+  
